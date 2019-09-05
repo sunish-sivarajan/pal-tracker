abd0b5601f5c898dea4772a17f9894107e79aab8
PAl-6 <pallintinareshkumar@gmail.com>
2019-09-05 15:18:56 +0530

    Health Check Implementation

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,7 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
- using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
 namespace PalTracker
 {
     public class Startup
@@ -36,6 +40,10 @@ namespace PalTracker
              //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();  
              services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
              services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+             services.AddCloudFoundryActuators(Configuration);
+             services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+             services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+             services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +60,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
-        }
+            app.UseCloudFoundryActuators();        }
        
 
     }
